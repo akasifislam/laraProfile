af8449a41e1279ef0f532a5b15599af8bae1bd4f
akasifislam <aseasifislam@gmail.com>
2021-03-25 18:18:45 +0600

    project profile

---
+++ routes/web.php
@@ -0,0 +1,74 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// backend routing
+
+
+
+Route::prefix('users')->group(function(){
+
+    Route::get('/view','Backend\UserController@view')->name('users.view');
+    Route::get('/add','Backend\UserController@add')->name('users.add');
+    Route::post('/store','Backend\UserController@store')->name('users.store');
+    Route::get('/edit/{id}','Backend\UserController@edit')->name('users.edit');
+    Route::post('/update/{id}','Backend\UserController@update')->name('users.update');
+    Route::get('/delete/{id}','Backend\UserController@delete')->name('users.delete');
+});
+Route::prefix('profiles')->group(function(){
+
+    Route::get('/store','Backend\ProfileController@view')->name('profiles.view');
+    Route::get('/view','Backend\ProfileController@edit')->name('profiles.edit');
+    Route::post('/add','Backend\ProfileController@update')->name('profiles.update');
+    Route::get('/password/view','Backend\ProfileController@passwordView')->name('profiles.password.view');
+    Route::post('/password/update','Backend\ProfileController@passwordUpdate')->name('profiles.password.update');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Route::prefix('sliders')->group(function(){
+//     Route::get('/view','Backend\UserController@view')->name('users.view');
+//     Route::get('/add','Backend\UserController@add')->name('users.add');
+//     Route::post('/store','Backend\UserController@store')->name('users.store');
+//     Route::get('/edit/{id}','Backend\UserController@edit')->name('users.edit');
+//     Route::post('/update/{id}','Backend\UserController@update')->name('users.update');
+//     Route::get('/delete/{id}','Backend\UserController@delete')->name('users.delete');
+// });
+
